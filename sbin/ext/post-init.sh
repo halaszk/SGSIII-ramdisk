7e37061b8ed762a961853ecea9101534a7d11e9a
halaszk <halaszk@deathlord.eu>
2013-01-20 22:53:42 +0100

    Ported CPU UV tab from AndreiLux’s STweaks config Dynamic generation of STweaks config and initial GPU voltages are set correctly (mostly based on AndreiLux’s uci.sh script interface)

--- sbin/ext/post-init.sh
+++ sbin/ext/post-init.sh
@@ -18,6 +18,7 @@ done;
 if [ ! -d /data/.siyah ]; then
 $BB mkdir -p /data/.siyah;
 fi;
+. /res/customconfig/customconfig-helper
 
 ccxmlsum=`md5sum /res/customconfig/customconfig.xml | awk '{print $1}'`
 if [ "a${ccxmlsum}" != "a`cat /data/.siyah/.ccxmlsum`" ];
@@ -26,12 +27,9 @@ then
   echo ${ccxmlsum} > /data/.siyah/.ccxmlsum;
 fi
 [ ! -f /data/.siyah/default.profile ] && cp /res/customconfig/default.profile /data/.siyah;
-[ ! -f /data/.siyah/battery.profile ] && cp /res/customconfig/battery.profile /data/.siyah/battery.profile;
-[ ! -f /data/.siyah/performance.profile ] && cp /res/customconfig/performance.profile /data/.siyah/performance.profile;
 
 $BB chmod 0777 /data/.siyah/ -R;
 
-. /res/customconfig/customconfig-helper;
 read_defaults;
 read_config;
 
@@ -144,8 +142,10 @@ $BB chmod 6755 /sbin/ext/cortexbrain-tune.sh;
 # apply STweaks settings
 echo "booting" > /data/.siyah/booting;
 pkill -f "com.gokhanmoral.stweaks.app";
-$BB sh /res/uci.sh restore;
-
+sleep 13
+export CONFIG_BOOTING=1
+$BB sh /res/uci.sh apply;
+export CONFIG_BOOTING=
 # restore all the PUSH Button Actions back to there location
 $BB mount -o remount,rw rootfs;
 $BB mv /res/no-push-on-boot/* /res/customconfig/actions/push-actions/;

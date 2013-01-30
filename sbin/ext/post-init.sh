8141b8f3852e48f164fe31a1a84180d2c34cdccc
halaszk <halaszk@deathlord.eu>
2013-01-30 01:32:55 +0100

    last commit function fixing / fix not working script / mods

--- sbin/ext/post-init.sh
+++ sbin/ext/post-init.sh
@@ -150,8 +150,10 @@ fi;
 # apply STweaks settings
 echo "booting" > /data/.siyah/booting;
 pkill -f "com.gokhanmoral.stweaks.app";
+sleep 13;
+export CONFIG_BOOTING=1;
 $BB sh /res/uci.sh restore;
-
+export CONFIG_BOOTING=
 # restore all the PUSH Button Actions back to there location
 $BB mount -o remount,rw rootfs;
 $BB mv /res/no-push-on-boot/* /res/customconfig/actions/push-actions/;
